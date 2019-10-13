9e7840b9072572408e0824f1dfda56c4cbd0c79f
Alexey Zakruzhetskyi <l500etc@gmail.com>
2019-10-13 22:01:42 +0300

    finish with landing page

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const $ = window.$
+
+$(document).ready(function () {
+  $('.navbar-toggler').click(function () {
+    $('.hamburger-container').toggleClass('open')
+  })
+
+  $('.nav-link').click(function (e) {
+    document.location.reload()
+    e.preventDefault()
+  })
+})
